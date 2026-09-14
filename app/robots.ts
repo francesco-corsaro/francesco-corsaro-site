b39d6ccc9808d502605b8d730d7ae9c22e9a8f41
francesco corsaro <57091974+francesco-corsaro@users.noreply.github.com>
2026-09-15 00:01:36 +0200

    Rafforza robots.txt e blocca gli endpoint API dalla scansione

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,18 @@
 import { MetadataRoute } from 'next';
 import { isPublicSite, site } from '@/lib/site';
 
-export default function robots():MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   if (!isPublicSite) {
     return { rules: { userAgent: '*', disallow: '/' } };
   }
 
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: `${site.url}/sitemap.xml`
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/'
+    },
+    sitemap: `${site.url}/sitemap.xml`,
+    host: site.url
   };
 }
